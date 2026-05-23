b7012eb2db4802ed3781b001da258363909cd8c2
Nitin Khankriyal <nitinkhankriyal125@gmail.com>
2026-05-23 09:04:10 +0530

    Added Questions

--- Basic_Programs.py
+++ Basic_Programs.py
@@ -2,6 +2,7 @@
 '''
 print("Hello world")
 '''
+###############################################################################################################################
 #Program to print the output of the arithmetic operators
 '''
 a=int(input("Enter the first Number : "))
@@ -15,6 +16,7 @@ print("The subtraction of the number is ",d)
 print("The multiplication of the number is ",e)
 print("The division of the number is ",f)
 '''
+################################################################################################################################
 #Program to convert Decimal number to number system
 '''
 a=int(input("Enter the  decimal number :"))
@@ -27,7 +29,9 @@ while a>0:
     a=a//10
 print("The number is ",sum)
 '''
+##################################################################################################################################
 #Write a program whether a number is the Palimdrome number of not
+'''
 a=int(input("Enter the Number :"))
 n=a
 sum=0
@@ -39,3 +43,114 @@ if a==sum:
     print("The number is the Palindrome Number")
 else:
     print("The number is not the Palindrome number")
+'''
+#################################################################################################################################
+#Write a program to check whether a number is the Armstrong Number or not
+'''
+a=int(input("Enter the number :"))
+n=a
+sum=0
+digit=0
+while n>0:
+    n=n//10
+    digit=digit+1
+n=a
+while n>0:
+    b=n%10
+    sum=pow(b,digit)+sum
+    n=n//10
+if a==sum:
+    print("The number is the armstrong Number")
+else:
+    print("The number is not the armstrong number")
+'''
+#################################################################################################################################
+
+'''Write a program to print the following pattern :
+
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+
+'''
+
+'''
+a=int(input("Enter the number of Rows :"))
+b=1
+print("The pattern is : \n")
+for i in range(a,0,-1):
+    for j in range(1,i):
+        print(" ",end=" ")
+    for j in range(0,b):
+        print("*",end=" ")
+    for k in range(0,b-1):
+        print("*",end=" ")
+    print("",end="\n")
+    b=b+1
+'''
+#################################################################################################################################
+
+'''Write a program to print the following pattern :
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+'''
+'''
+a=int(input("Enter the number of Rows :"))
+print("The pattern is : \n")
+for i in range(1,a+1):
+    for j in range(0,i):
+        print("*",end=" ")
+    print("",end="\n")
+
+'''
+#############################################################################################################################
+
+'''Write the program to print the following pattern :
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+'''
+'''
+a=int(input("Enter the number of Rows :"))
+
+print("The pattern is : \n")
+for i in range(a,0,-1):
+    for j in range(0,i):
+        print("*",end=" ")
+    print("",end="\n")
+
+'''
+################################################################################################################################
+
+'''Write the program to print the following pattern
+
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        * 
+
+'''
+a=int(input("Enter the number of Rows :"))
+b=0
+print("The pattern is : \n")
+for i in range(a,0,-1):
+    for j in range(0,b):
+        print(" ",end=" ")
+    for j in range(0,i):
+        print("*",end=" ")
+    for k in range(0,i-1):
+        print("*",end=" ")
+    print("",end="\n")
+    b=b+1
