0cef179fa3e72e5f47f7c7dceaf78709f2a10ec6
Nitin Khankriyal <nitinkhankriyal125@gmail.com>
2026-05-24 23:15:32 +0530

    Added more Questions

--- Basic_Programs.py
+++ Basic_Programs.py
@@ -344,7 +344,116 @@ Given Input: str1 = "PYnative"
 
 Expected Output: evitanYP
 '''
+'''
 a=input("Enter the string : ")
 c=a[-1::-1]
 print("The reverse string is :",c)
+'''
 ###############################################################################################################################
+'''Practice Problem: Write a single list comprehension that generates a list of all prime numbers up to a given number n.
+
+Prime number is a whole number greater than 1 that cannot be exactly divided by any whole number other than 
+itself and 1 (e.g. 2, 3, 5, 7, 11).
+
+Exercise Purpose: This exercise pushes your List Comprehension skills to the limit. It requires nesting logic and 
+understanding the mathematical definition of a prime (a number x > 1 that has no divisors other than 1 and itself).
+
+Given Input: n = 20
+
+Expected Output: Primes: [2, 3, 5, 7, 11, 13, 17, 19]
+'''
+'''
+def isprime(i):
+    count=0
+    for j in range(2,i):
+        if(i%j==0):
+            count=1
+        else:
+            continue
+    if count==0:
+        return 1
+    else:
+        return 0
+b=[]
+a=int(input("Enter the number : "))
+for i in range(2,a):
+    if(isprime(i)==1):
+        b.append(i)
+    else:
+        continue
+print(b)
+'''
+############################################################################################################################
+'''
+Practice Problem: Given a list of numbers, push all zeros to the end of the list while maintaining the relative 
+order of all non-zero elements. This must be done efficiently.
+
+Exercise Purpose: This “Stable Partitioning” problem is a favorite in technical interviews. It tests your ability 
+to filter data based on a specific criterion while preserving the integrity of the remaining sequence.
+
+Given Input: List: [0, 1, 0, 3, 12]
+
+Expected Output: Result: [1, 3, 12, 0, 0]
+'''
+'''
+a=[]
+l=int(input("Enter the length of the list : "))
+c=l-1
+for i in range(0,l):
+    d=int(input("Enter the number : "))
+    a.append(d)
+for i in range(0,a.count(0)):
+    b=a.index(0)
+    for j in range(b,l-1-i):
+        a[j]=a[j+1]
+    a[c]=0
+    c=c-1
+print(a)
+'''
+################################################################################################################################
+'''Practice Problem: Create a function that takes a list and an integer N, and breaks the list into smaller sublists, 
+each of length N. The last chunk may be shorter if the list length isn’t perfectly divisible by N.
+
+Exercise Purpose: Batch processing is essential when dealing with large 
+datasets or API limits (e.g., “send 50 emails at a time”). This exercise demonstrates the use of the Step 
+Parameter in the range() function.
+
+Given Input:
+
+List: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+N: 3
+Expected Output:
+
+Chunks: [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]]
+'''
+'''
+def list_slice(a,n):
+    l=[]
+    j=0
+    
+    k=n
+    b=len(a)
+    c=b//n
+    p=c
+    if b%n!=0:
+        p=c+1
+    for i in range(0,c):
+        d=a[j:k]
+        l.append(d)
+        j=k
+        k=k+n
+    if j!=b:
+        d=a[j:]
+        l.append(d)
+    print("[",end=" ")
+    for i in range(0,p):
+        if i!=p-1:
+            print(l[i],",",end=" ")
+        else:
+            print(l[i],end=" ")
+    print("]")
+a=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list_slice(a,3)
+
+'''
+#################################################################################################################################
